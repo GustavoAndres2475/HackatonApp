2dc722915d74f12d9843f061a21127688860493b
GustavoAndres2475 <gaaguayo@ucsd.edu>
2018-10-13 10:38:19 -0700

    commit

---
+++ listPair.java
@@ -0,0 +1,23 @@
+/*
+ * Gustavo Aguayo
+ * Jason Chesemore 
+ * Jonathan Major
+ */
+
+/**
+ * Class creates the object used to store people and the respective money owed/owe
+ */
+
+public class listPair{
+
+	public String person;
+	public int money;
+
+	public listPair(String person, int money){
+
+		this.person = person;
+		this.money = money;
+
+	}
+}
+
--- trackDebt.java
+++ trackDebt.java
@@ -1,16 +1,42 @@
 /* 
  * Gustavo Aguayo
  * Jason Chesemore
- * Derek Nguyen
  * Jonathan Major
  */
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class trackDebt {
 
-	protected List<String> person;
-  protected List<Integer> money;
-  
+	/* instance variables */
+  int quantity;														// Quantity 
+  boolean owed;														// Checks if quantity is owned or owned
+  boolean adding;													// Checks if user wants to add more people to the list
 
+	/*
+   * Method appends one more element to the list based on user input
+	 *
+	 * @param - name: the name of the person to add to the list
+	 */
+	public int addToList( String name, int money){
+
+		List<listPair> data = new ArrayList<listPair>();				         
+
+		/* loop always is running waiting for the user's action */
+		adding = false;	
+		while(true) {
+			
+			/* when the user wishes to add someone to the list this should be triggered */
+			if(adding = true){
+				data.add(new listPair(name, money));
+				/* reset the if to be false */
+				adding = false;										
+				return 1;
+			}
+			else{
+				return 0;
+			}
+		}
+	}
 }
