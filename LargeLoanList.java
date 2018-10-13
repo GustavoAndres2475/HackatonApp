ea396facf6608419fb569e1bca4daec8062f677c
Jason Chesemore <jasonchesemore@sbcglobal.net>
2018-10-13 10:54:50 -0700

    Working on LargeLoanList

---
+++ LargeLoanList.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+
+public class LargeLoanList {
+	
+	public static void main (String[] args) {
+	List<LargeLoanObject> test = new ArrayList<LargeLoanObject>();
+	LargeLoanObject temp = new LargeLoanObject("HI", 100000.20, 1.2, 1000);
+	
+	System.out.println(temp);
+
+	
+	}
+}
